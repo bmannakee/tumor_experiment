3200e6d356bc63bbd62e41ebaac4348bdb0aa62c
Brian Mannakee <brianmannakee@Brians-iMac.local>
2018-08-08 10:20:41 -0700

    save experiment

---
+++ tumor_experiment/experiment.py
@@ -0,0 +1,9 @@
+# Create and submit jobs to run simulation experiments on the UA HPC
+# Written and tested on Python 3, but will likely work on 2.7
+
+class Experiment:
+    """ Class that defines a single simulation experiment """
+    def __init__(self, experiment_dir ):
+        self.out_dir = experiment_dir
+        """ TODO: get variants from tumorsim """
+        
